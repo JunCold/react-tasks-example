81ffeab68c6a67408557f4d333be3740f97f7ec9
JunCode <109869846+JunCold@users.noreply.github.com>
2023-11-02 18:50:13 -0600

    first commit

---
+++ src/components/TaskList.jsx
@@ -0,0 +1,20 @@
+import {taskContext} from '../context/taskContext';
+import TaskCard from './TaskCard'
+import { useContext } from "react";
+
+
+function TaskList() {
+
+    const {tasks}= useContext(taskContext)
+  if (tasks.length === 0) {
+    return <h1 className='text-white text-4xl font-bold text-center'>No hay tareas aún </h1>
+  }
+  return(
+  <div className='grid grid-cols-4 gap-2'>
+    {tasks.map((task) => (
+       <TaskCard key={task.id} task={task} />
+    ))}
+  </div>
+  );
+}
+export default TaskList;
